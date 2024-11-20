765e2efc9d48e0973edb5ce95445d1ed542af134
Mukesh Eswaran <97874308+MukeshEswaran@users.noreply.github.com>
2024-11-19 20:28:42 -0600

    Add files via upload

---
+++ Hands_on13.py
@@ -0,0 +1,114 @@
+class Graph:
+    def __init__(self, vertices):
+        self.graph = {i: [] for i in range(vertices)}
+        self.V = vertices
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def topological_sort_util(self, v, visited, stack):
+        visited[v] = True
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.topological_sort_util(i, visited, stack)
+        stack.insert(0, v)
+
+    def topological_sort(self):
+        visited = [False] * self.V
+        stack = []
+        for i in range(self.V):
+            if not visited[i]:
+                self.topological_sort_util(i, visited, stack)
+        return stack
+
+# Test
+g = Graph(6)
+g.add_edge(5, 2)
+g.add_edge(5, 0)
+g.add_edge(4, 0)
+g.add_edge(4, 1)
+g.add_edge(2, 3)
+g.add_edge(3, 1)
+print("Topological Sort:", g.topological_sort())
+
+
+class GraphDFS:
+    def __init__(self, vertices):
+        self.graph = {i: [] for i in range(vertices)}
+        self.V = vertices
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def dfs_util(self, v, visited):
+        visited[v] = True
+        print(v, end=" ")
+        for neighbor in self.graph[v]:
+            if not visited[neighbor]:
+                self.dfs_util(neighbor, visited)
+
+    def dfs(self, start):
+        visited = [False] * self.V
+        print("DFS starting from vertex", start, ":")
+        self.dfs_util(start, visited)
+
+# Test
+g = GraphDFS(6)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 4)
+g.add_edge(4, 5)
+g.dfs(0)
+
+
+class Kruskal:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.edges = []
+
+    def add_edge(self, u, v, weight):
+        self.edges.append((weight, u, v))
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    def kruskal(self):
+        self.edges.sort()
+        parent = []
+        rank = []
+        result = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        for weight, u, v in self.edges:
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                result.append((u, v, weight))
+                self.union(parent, rank, x, y)
+        return result
+
+# Test
+g = Kruskal(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+print("Kruskal's MST:", g.kruskal())
+
+
